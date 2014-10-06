4b2b6c5183de3eb1a4b2a3cc40c151807442e5bc
Pierre Chaussalet <pierre.chaussalet@in-tact.fr>
2014-10-06 18:05:23 +0200

    Add verbose mode

--- src/client/client.go
+++ src/client/client.go
@@ -13,21 +13,49 @@ import (
 	"encoding/json"
 )
 
-func List(host string) {
+var debugEnabled = false
+
+func debug(message string, args ...interface{}) {
+	if debugEnabled {
+		fmt.Printf(message, args...)
+	}
+}
+
+func fail() {
+	debug("[FAIL]\n")
+}
+
+func success() {
+	debug("[OK]\n")
+}
+
+func List(host string, verbose bool) {
+	debugEnabled = verbose
+	debug("Calling http://%v endpoint...", host)
 	resp, err := http.Get("http://" + host)
 	if err != nil {
+		fail()
 		log.Fatalln(err)
 	}
+	success()
+	debug("Parsing remote response...")
 	var containers []string
 	err = json.NewDecoder(resp.Body).Decode(&containers)
 	if err != nil {
+		fail()
 		log.Fatalln(err)
 	}
-	sep := "\n  "
-	fmt.Printf("Containers :" + sep + "%v\n", strings.Join(containers, sep))
+	success()
+	if len(containers) > 0 {
+		sep := "\n  "
+		fmt.Printf("Containers :" + sep + "%v\n", strings.Join(containers, sep))
+	} else {
+		fmt.Println("No container running on remote host.")
+	}
 }
 
-func Deploy(command []string, templateFile, varsFile, host string) {
+func Deploy(command []string, templateFile, varsFile, host string, verbose bool) {
+	debugEnabled = verbose
 	component := command[1]
 	targetEnv := command[2]
 	version := ""
@@ -35,49 +63,82 @@ func Deploy(command []string, templateFile, varsFile, host string) {
 		version = command[3]
 	}
 
+	if (verbose) {
+		debug("Deploy agruments : component=%v - environment=%v - version=%v\n", component, targetEnv, version)
+	}
+
+	debug("Reading template file %v...", templateFile)
 	templateRaw, err := ioutil.ReadFile(templateFile)
 	if err != nil {
+		fail()
 		log.Fatalln(err)
 	}
 	template := string(templateRaw)
+	success()
 
+	debug("Reading values file %v...", varsFile)
 	varsString, err := ioutil.ReadFile(varsFile)
 	if err != nil {
+		fail()
 		log.Fatalln(err)
 	}
+	success()
+
+	debug("Parsing values file...")
 	m := make(map[string]map[string]map[string]interface {})
 	err = yaml.Unmarshal(varsString, &m)
 	if err != nil {
+		fail()
 		log.Fatalln(err)
 	}
+	success()
+
+	debug("Extracting values for environment...")
 	vars := make(map[string]string, len(m["global"]) + len(m[targetEnv]))
 	vars["VERSION"] = version
 	vars = extractVars(vars, m, "global", component)
 	vars = extractVars(vars, m, targetEnv, component)
+	success()
+
+	debug("Replacing values in template...")
 	for key, value := range vars {
 		placeholder := fmt.Sprintf("{{%v}}", key)
 		template = strings.Replace(template, placeholder, value, -1)
 	}
+	success()
+
+	debug("Parsing generated config file...")
 	configs := make(map[string]map[string]interface {})
 	err = yaml.Unmarshal([]byte(template), &configs)
 	config := configs[component]
 	config["name"] = fmt.Sprintf("%v_%v_%v", vars["project"], targetEnv, component)
 	payload, err := yaml.Marshal(config)
 	if err != nil {
+		fail()
 		log.Fatalln(err)
 	}
+	success()
+	debug("Generated config :\n%v", string(payload))
+
+	debug("Calling remote server http://%v/new...", string(host))
 	resp, err := http.Post("http://"+string(host)+"/new", "application/x-yaml", strings.NewReader(string(payload)))
 	if err != nil {
+		fail()
 		log.Fatalln(err)
 	}
+	success()
+
+	debug("Extracting container id...")
 	p := make([]byte, resp.ContentLength)
 	resp.Body.Read(p)
 	body := string(p)
 	body = strings.Replace(body, "\"", "", -1)
 	id, err := base64.StdEncoding.DecodeString(body)
 	if err != nil {
+		fail()
 		log.Fatalln(err)
 	}
+	success()
 	fmt.Print(string(id))
 }
 
--- src/rweave.go
+++ src/rweave.go
@@ -6,6 +6,8 @@ import (
 
 	client "./client"
 	server "./server"
+	"fmt"
+	"strings"
 )
 
 func main() {
@@ -14,10 +16,14 @@ func main() {
 	host := flags.String("h", "127.0.0.1:14243", "Server address in the form ip:port (Client and Daemons mode)")
 	templateFile := flags.String("t", "", "Template file location (Client mode)")
 	varsFile := flags.String("v", "", "Variables file location (Client mode)")
+	verbose := flags.Bool("verbose", false, "Run in verbose mode (Client mode)")
 	flags.Parse(os.Args[1:])
 	if *daemon {
 		server.Start(*host)
 	} else {
+		if *verbose {
+			fmt.Println("Verbose mode = ON")
+		}
 		command := flags.Args()
 		if len(command) == 0 {
 			flags.PrintDefaults()
@@ -25,13 +31,13 @@ func main() {
 		}
 		switch command[0] {
 		case "list":
-			client.List(*host)
+			client.List(*host, *verbose)
 		case "deploy":
 			if len(command) < 3 || *templateFile == "" || *varsFile == "" {
 				flags.PrintDefaults()
 				return
 			}
-			client.Deploy(command, *templateFile, *varsFile, *host)
+			client.Deploy(command, *templateFile, *varsFile, *host, *verbose)
 		}
 	}
 }
